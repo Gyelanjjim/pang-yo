9f85f545f36dd54eb86f67407eda9c25c0611c2a
Gyelanjjim <songcj92@gmail.com>
2023-06-22 14:21:54 +0900

    [Update] 배포주소변경-3

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-15-164-212-140.ap-northeast-2.compute.amazonaws.com",
+      "ec2-13-124-203-90.ap-northeast-2.compute.amazonaws.com",
     ],
   },
   experimental: {
