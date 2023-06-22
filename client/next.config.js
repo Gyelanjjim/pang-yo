6ff20503d695693e305fc55423734badd8735164
Gyelanjjim <songcj92@gmail.com>
2023-06-22 17:38:35 +0900

    [Update]배포 환경설정

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-13-124-203-90.ap-northeast-2.compute.amazonaws.com",
+      "ec2-52-78-56-64.ap-northeast-2.compute.amazonaws.com",
     ],
   },
   experimental: {
